25cccd0e6bc151160303e8d5942cc6a71996419f
milinbhakta <desire2dosomething@gmail.com>
2016-01-15 15:15:32 +0530

    version 1.0

---
+++ svm.py
@@ -0,0 +1,12 @@
+from sklearn.svm import SVC
+import numpy as np
+X = np.array([[0, 90], [91, 180], [181,270], [271,360]])
+y = np.array([1,2,3,4])
+clf = SVC(kernel='linear')
+clf.fit(X, y)
+a = input("Enter The Angle")
+#b = input("Enter Maximum Angle")
+p = clf.predict([[a,a]])
+
+print p
+#print '%d is lies in quadrant %d' %(a,p[0])
